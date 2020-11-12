d65969eba559abe252b9e5f64a5dc0619eada5bc
Loren <lorensansol@gmail.com>
2020-11-12 06:25:48 -0300

    Actualización: 2020-11-12 06:25:48

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -135,19 +135,7 @@ gulp.task('fa-min',
   run(`cat node_modules/@fortawesome/fontawesome-free/js/{brands,solid}.js | grep -Eo "^\s{4}\"($usedFaIcons)\".+" | perl -pe "s/\]$/\],/gm" | sed '1s/^/{\'$'\n/g' | sed '$s/,$/}/g' > _assets/js/_includes/fa-used.json`)
 )
 
-gulp.task('fonts', (done) => {
-  return gulp
-    .src('_assets/fonts/*.{ttf,otf}')
-    .pipe(fontgen({
-      css_fontpath: '/assets/fonts',
-      // css: '_assets/css/_sass/fonts',
-      dest: 'assets/fonts'
-    }))
-  // child.exec('mv ./assets/fonts/*.css ./_assets/css/_sass/fonts/')
-  done()
-})
-
-gulp.task('fon', gulp.series(
+gulp.task('fonts', gulp.series(
   () => {
     return gulp
       .src('_assets/fonts/*.{ttf,otf}')
